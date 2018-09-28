3fa07134a56deca3c5775beada89adbd1d7e5e38
Stevy <664553716@qq.com>
2018-09-29 00:03:13 +0800

    1

--- apps/user/models.py
+++ apps/user/models.py
@@ -2,7 +2,27 @@ from django.db import models
 
 
 # Create your models here.
-class UserProfile(models.Model):
-	name = models.CharField(max_length=10)
-	email = models.EmailField(max_length=100, null=True, blank=True, verbose_name="邮箱")
+class User(models.Model):
+	user_name = models.CharField(max_length=64, default='null_name', verbose_name='姓名')
+	user_gender = models.CharField(max_length=2, default='male', choices=(('female', u'女'), ('male', '男'),), verbose_name='性别')
+	user_age = models.IntegerField(null=True, verbose_name='年龄')
+	user_email = models.EmailField(max_length=64, null=True, verbose_name="邮箱")
 
+
+class Role(models.Model):
+	role_name = models.CharField(max_length=64, unique=True, default='null_name', verbose_name='角色名')
+	role_code = models.CharField(max_length=64, unique=True)
+	role_description = models.CharField(max_length=1024, null=True, verbose_name='角色描述')
+
+
+class UserRole(models.Model):
+	user_id = models.IntegerField(verbose_name='用户id')
+	role_codes = models.CharField(max_length=256, default=None, blank=False, verbose_name='用户角色code')
+
+
+class RolePermission(models.Model):
+	role_code = models.CharField(max_length=64, blank=True, verbose_name='用户角色标识')
+	pm_code = models.CharField(max_length=64, blank=False, verbose_name='权限标识')
+	
+	class Mete:
+		unique_together = ('role_code', '')
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from user.models import UserProfile
+from user.models import User
 from user.serializers import UserProfileSerializer
 from rest_framework import generics
 
 
 # Create your views here.
 class UserProfileList(generics.ListCreateAPIView):
-	queryset = UserProfile.objects.all()
+	queryset = User.objects.all()
 	serializer_class = UserProfileSerializer
 
