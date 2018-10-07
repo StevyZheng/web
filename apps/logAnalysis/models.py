da02fd8a17aa2195d56472f8b764826dc7afb7bb
Stevy <664553716@qq.com>
2018-10-07 23:46:50 +0800

    2.10

--- apps/logAnalysis/models.py
+++ apps/logAnalysis/models.py
@@ -1,5 +1,7 @@
 # coding = utf-8
 from django.db import models
+from web.settings import FILE_PATH_FIELD_DIRECTORY
+from user.models import User
 
 
 # Create your models here.
@@ -9,3 +11,8 @@ class LogDatabase(models.Model):
 	err_keyword = models.CharField(max_length=128, verbose_name='错误信息关键字')
 	solution = models.TextField(verbose_name='解决方案')
 
+
+class LogFile(models.Model):
+	log_name = models.CharField(max_length=126, verbose_name='日志文件名')
+	log_path = models.FilePathField(path=FILE_PATH_FIELD_DIRECTORY, verbose_name='文件路径')
+	upload_user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
